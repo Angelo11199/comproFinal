ca73bde439b7fee91fefa888df0aa7093b9fb54f
iceCream <142316765+Angelo11199@users.noreply.github.com>
2023-11-22 08:20:33 -0500

    changes in main cpp

--- guides/contactMangagementSystem.md
+++ guides/contactMangagementSystem.md
@@ -1,4 +1,6 @@
 # Contact Management system
+### assigned
+1. Ice cream
 ## Description on PDF
 The contact management system and bank management system both are quite similar.
 You can effortlessly develop a system where you will be able to **incorporate** and **delete** contact details. The system will let customers **add** contacts, with **names** and **email addresses**. The end result of this project will be an application similar to a phonebook application. This project will
--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,17 @@
 #include "../include/snippets.h"  //ALWAYS ADD THIS
 using namespace std;
+#include <conio.h>  // Include the <conio.h> header file
+
+#include <iostream>
+
+using namespace std;
+
 int main() {
+    cout << "----------------------------------------------" << endl;
+    cout << "|| Welcome to the Contact Management System ||" << endl;
+    cout << "----------------------------------------------" << endl;
+    cout << "Press any key to continue...";
+    getch();
+
     return 0;
 }
