11f96e760ad950a5fa6edcc2d9f19cc4e432a965
Gian <202311131@fit.edu.ph>
2023-11-25 00:35:41 +0800

    testing ni gian

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,19 @@
 #include "../include/snippets.h"  //ALWAYS ADD THIS
+#include <map>
 using namespace std;
+struct Account {
+    string name, address, accountType;
+    double amount;
+};
+
 int main() {
-    print ("hi");
+    cout << "Welcome to Gian's & Elijah's Bank Management System!\n";
+    cout << "Do you want to open an account? (yes/no): ";
+    cin >> answer;
+    
+    if (answer == "yes") {
+        cout << "Kindly enter your"
+    }
+    print ("");
     return 0;
 }
