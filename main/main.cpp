2d56b868989f5ec363c77f87afdfb4de73e07d72
Rye <142316765+Angelo+11199@users.noreply.github.com>
2023-11-24 21:16:09 +0800

    eads

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,20 @@
 #include "../include/snippets.h"  //ALWAYS ADD THIS
+#include <iomanip>
 using namespace std;
+void options()
+{
+
+}
+
 int main() {
-    string name, course, year;
-    int number;
-    print ("Student Record Management System");
-    getch();
+    string first, middle, last, course, year;
+    int number, a;
+    print("Student Record Management System");
+    print("1 - Add record(s)");
+    print("2 - Delete record(s)");
+    print("3 - Modify records");
+    print("4 - View records");
+    print("5 - Exit");
 
     return 0;
 }
