f42a77671fd98254669f9f8ae25c8f067ccd58de
Rye <142316765+Angelo+11199@users.noreply.github.com>
2023-11-25 23:36:20 +0800

    ewase

--- main/main.cpp
+++ main/main.cpp
@@ -4,20 +4,38 @@
 #include "../include/snippets.h"  //ALWAYS ADD THIS
 using namespace std;
 
+void addrecord(){
+    print("\nAdd a record");
+    string first = getStr("Enter first name: ");
+    string middle = getStr("Enter middle name (optional): ");
+    string last = getStr("Enter last name: ");
+    string roll = getStr("Enter roll number: ");
+    string year = getStr("Enter year level (number): ");
+    string course = getStr("Enter course: ");
+    string email = getStr("Enter email: ");
+    string fullname = last + ", " + first + " " + middle;
+    print ("Record added successfully!");
+    
+    return;
+}
+
+
 int main() {
+    int x = 0;
+    while (x == 0){
     print("Student Record Management System");
     print("1 - Add record(s)");
     print("2 - Delete record(s)");
-    print("3 - Modify records");
-    print("4 - View records");
+    print("3 - Modify record(s)");
+    print("4 - View record(s)");
     print("5 - Exit");
     string choice = getStr("Enter your choice: ");
+
     switch (choice[0]) {
         case '1':
-            print("Add record(s)");
+            addrecord();
             break;
         case '2':
-            print("Delete record(s)");
             break;
         case '3':
             print("Modify record(s)");
@@ -26,12 +44,12 @@ int main() {
             print("View record(s)");
             break;
         case '5':
+            x = 1;
             print("Exit");
             break;
         default:
             print("Invalid choice");
     }
-
-
+    }
     return 0;
 }
