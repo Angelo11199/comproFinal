a9f67ce44d70ffde7beafa3b561690f1c43a8de9
iceCream <142316765+Angelo11199@users.noreply.github.com>
2023-11-25 00:43:12 -0500

    ''

--- main/main.cpp
+++ main/main.cpp
@@ -1,18 +1,35 @@
-#include "../include/snippets.h"  //ALWAYS ADD THIS
 #include <iomanip>
 #include <iostream>
+
+#include "../include/snippets.h"  //ALWAYS ADD THIS
 using namespace std;
 
 int main() {
-    string choice;
     print("Student Record Management System");
     print("1 - Add record(s)");
     print("2 - Delete record(s)");
     print("3 - Modify records");
     print("4 - View records");
     print("5 - Exit");
-    cin >> choice;
-
-
+    string choice = getStr("Enter your choice: ");
+    switch (choice[0]) {
+        case '1':
+            print("Add record(s)");
+            break;
+        case '2':
+            print("Delete record(s)");
+            break;
+        case '3':
+            print("Modify record(s)");
+            break;
+        case '4':
+            print("View record(s)");
+            break;
+        case '5':
+            print("Exit");
+            break;
+        default:
+            print("Invalid choice");
+    }
     return 0;
 }
