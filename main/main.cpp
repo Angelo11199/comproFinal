1318be6cc62bfc7ea3669ac542f73b69759cab4b
iceCream <142316765+Angelo11199@users.noreply.github.com>
2023-11-25 00:45:23 -0500

    added guide on using struct.

--- main/main.cpp
+++ main/main.cpp
@@ -1,19 +1,26 @@
-#include "../include/snippets.h"  //ALWAYS ADD THIS
 #include <map>
+
+#include "../include/snippets.h"  //ALWAYS ADD THIS
 using namespace std;
 struct Account {
     string name, address, accountType;
     double amount;
 };
+/*
+    How to use Struct:
+        Account a;
+        a.name = "Gian";
+        a.address = "Bacoor";
+        a.accountType = "Savings";
+        a.amount = 1000;
 
+*/
 int main() {
     cout << "Welcome to Gian's & Elijah's Bank Management System!\n";
-    cout << "Do you want to open an account? (yes/no): ";
-    cin >> answer;
-    
+    string answer = getStr("Do you want to open an account? (yes/no):");
     if (answer == "yes") {
-        cout << "Kindly enter your"
+        cout << "Kindly enter your";
     }
-    print ("");
+    print("");
     return 0;
 }
