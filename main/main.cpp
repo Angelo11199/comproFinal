2da85e1e91fd66203b5cb6881c392903de4810d5
Gian <202311131@fit.edu.ph>
2023-11-25 16:49:09 +0800

    Wow may progress si gian

--- main/main.cpp
+++ main/main.cpp
@@ -1,19 +1,36 @@
 #include "../include/snippets.h"  //ALWAYS ADD THIS
 #include <map>
+#include <string>
 using namespace std;
+
 struct Account {
     string name, address, accountType;
     double amount;
 };
 
 int main() {
+    string answer;
     cout << "Welcome to Gian's & Elijah's Bank Management System!\n";
     cout << "Do you want to open an account? (yes/no): ";
     cin >> answer;
     
     if (answer == "yes") {
-        cout << "Kindly enter your"
+        Account newAccount;
+        printLn("Great! Let's get started with your account creation process.");
+        newAccount.name = getStr("Please enter your name: \n");
+        newAccount.address = getStr("Enter your address: \n");
+        newAccount.accountType = getStr("Enter your account type: \n");
+        cout << "Enter amount: \n";
+        cin >> newAccount.amount;
+        print("Account created successfully!");
+
+    } else if (answer == "no") {
+        cout << "No problem. Let us know if you changed your mind.\n";
+    } else {
+        cout << "Please answer in 'yes' or 'no' only. Otherwise it will be considered as invalid.\n";
     }
-    print ("");
+
+
+    
     return 0;
 }
