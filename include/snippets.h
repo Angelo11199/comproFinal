39106e660d5e834a02e82c22d9d8a136e63298e0
iceCream <142316765+Angelo11199@users.noreply.github.com>
2023-11-29 22:51:23 -0500

    updated

--- include/snippets.h
+++ include/snippets.h
@@ -5,6 +5,10 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#define SEPERATOR " | "
+// Search data here
+std::unordered_map<std::string, std::vector<std::string>> csvData;
+
 // prints in a new line
 template <typename T>
 void print(T Value) {
@@ -36,7 +40,7 @@ double getNum(std::string prompt = "") {
 std::string getStr(std::string prompt = "") {
     std::string s;
     std::cout << prompt;
-    // std::cin.ignore(); 
+    // std::cin.ignore();
     getline(std::cin, s);
     return s;
 }
@@ -51,8 +55,20 @@ bool readFile(std::string fileName, std::string& content) {
         }
         file.close();
         return true;
-    } else
+    } else {
+        print("File not found! creating file...");
+        std::ofstream file;
+        file.open(fileName);
+        if (file.is_open()) {
+            print("File created successfully!");
+            file << "sep=" + std::string(SEPERATOR) + "\n";
+            file.close();
+            return true;
+        } else {
+            return false;
+        }
         return false;
+    }
 }
 
 // adds data to the file. returns true if successful
@@ -78,8 +94,6 @@ void splitData(std::string str, std::string delimiter, std::vector<std::string>&
     }
     vec.push_back(str);
 }
-std::unordered_map<std::string, std::vector<std::string>>
-    csvData;
 
 std::vector<std::string> getRow(const std::string& value) {
     std::vector<std::string> contacts;
@@ -98,7 +112,7 @@ bool deleteRow(std::string fileName, std::string rowName) {
     // loop through the data and add the indexes element to the hashmap
     for (int i = 0; i < data.size() - 1; i++) {
         std::vector<std::string> row;
-        splitData(data[i], ",", row);
+        splitData(data[i], SEPERATOR, row);
         if (row[0] == rowName) {
             data.erase(data.begin() + i);
             std::string newContent;
@@ -126,53 +140,49 @@ bool updateRow(std::string fileName, std::string colName, std::string newValue, 
     splitData(contents, "\n", data);
     for (int i = 0; i < data.size() - 1; i++) {
         std::vector<std::string> row;
-        splitData(data[i], ",", row);
-        if (row[0] == colName) {
-            row[indexToUpdate] = newValue;
-            data[i] = "";
-            for (int j = 0; j < row.size() - 1; j++) {
-                data[i] += row[j] + ",";
-            }
-            data[i] += row[row.size() - 1];
-            std::string newContent;
-            for (int j = 0; j < data.size() - 1; j++) {
-                newContent += data[j] + "\n";
-            }
-            std::ofstream file;
-            file.open(fileName);
-            if (file.is_open()) {
-                file << newContent;
-                file.close();
-                return true;
-            } else {
-                return false;
-            }
-        }
+        splitData(data[i], SEPERATOR, row);
+        if (row[0] != colName) continue;
+        row[indexToUpdate] = newValue;
+        data[i] = "";
+        for (int j = 0; j < row.size() - 1; j++) data[i] += row[j] + SEPERATOR;
+        data[i] += row[row.size() - 1];
+        std::string newContent;
+        for (int j = 0; j < data.size() - 1; j++) newContent += data[j] + "\n";
+        std::ofstream file;
+        file.open(fileName);
+        if (!file.is_open()) return false;
+        i == 0 && file << "sep=" + std::string(SEPERATOR) + "\n";
+        file << newContent;
+        file.close();
+        return true;
     }
     return true;
 }
+void pauseProgram() {
+    print("press any key to continue...");
+    getch();
+}
+
 // initializes the csvData. Args : filename, the hashmap to store the data, the indexes used for searching
 void init(std::string content, std::unordered_map<std::string, std::vector<std::string>>& csvData, const std::vector<int>& indexes) {
     std::string contents;
+    print("Initializing...");
     readFile(content, contents)
         ? print(content + " read successfully.")
         : print(content + " read failed.");
     std::vector<std::string> data;
-    splitData(contents, "\n", data);
     std::vector<std::string> fields;
-    // loop through the data and add the indexes element to the hashmap
-    for (int i = 0; i < data.size() - 1; i++) {
+    splitData(contents, "\n", data);
+    if (data[0].substr(0, 4) == "sep=") {
+        data.erase(data.begin());
+    }
+    for (int i = 0; i < data.size(); i++) {
         std::vector<std::string> row;
-        splitData(data[i], ",", row);
-        for (int j = 0; j < indexes.size(); j++) {
+        if (data[i].empty()) continue;
+        splitData(data[i], SEPERATOR, row);
+        for (int j = 0; j < indexes.size(); ++j) {
             csvData[row[indexes[j]]] = row;
         }
     }
-    print("Initializing...");
     print("Initialization complete.");
-}
-
-void pauseProgram() {
-    print("press any key to continue...");
-    getch();
 }
--- main/main.cpp
+++ main/main.cpp
@@ -1,122 +1,155 @@
 #include <map>
 #include <string>
 
-#include "../include/snippets.h" //ALWAYS ADD THIS
+#include "../include/snippets.h"  //ALWAYS ADD THIS
 using namespace std;
-
+const string fileName = "accounts.csv";
 struct Account {
     string name, address, accountType;
     double amount;
 };
 
 // Function to show account details
-void showAccountDetails(const Account& acc) {
-    print("Name: " + acc.name);
-    print("Address: " + acc.address);
-    print("Account Type: " + acc.accountType);
-    print("Balance: $" + to_string(acc.amount));
+void showAccountDetails(vector<string> accounts) {
+    print("Name: " + accounts[0]);
+    print("Address: " + accounts[1]);
+    print("Account Type: " + accounts[2]);
+    print("Balance: $" + accounts[3]);
 }
 
 // Function to deposit funds
-void deposit(Account& acc, double amount) {
+void deposit(vector<string> account, double amount) {
     if (amount > 0) {
-        acc.amount += amount;
-        print("Deposit successful. New balance: $" + to_string(acc.amount));
-    } else {
+        double balance = stoi(account[3]);
+        balance += amount;
+        print("Deposit successful. New balance: $" + to_string(balance));
+        updateRow(fileName, account[0], to_string(balance), 3);
+        csvData[account[0]][3] = to_string(balance);
+    } else
         print("Invalid deposit amount. Please enter a positive amount.");
-    }
 }
 
 // Function to withdraw funds
-void withdraw(Account& acc, double amount) {
-    if (amount > 0 && amount <= acc.amount) {
-        acc.amount -= amount;
-        print("Withdrawal successful. New balance: $" + to_string(acc.amount));
+void withdraw(vector<string> acc, double amount) {
+    double balance = stoi(acc[3]);
+    if (amount > 0 && amount <= balance) {
+        balance -= amount;
+        print("Withdrawal successful. New balance: $" + to_string(balance));
+        updateRow(fileName, acc[0], to_string(balance), 3);
+        csvData[acc[0]][3] = to_string(balance);
     } else {
         print("Invalid withdrawal amount or insufficient funds.");
     }
 }
-
-// Function to search for an account by name
-void searchContact(const map<string, Account>& accounts, const string& accountName) {
-    auto it = accounts.find(accountName);
-    if (it != accounts.end()) {
-        Account selectedAccount = it->second;
-        showAccountDetails(selectedAccount);
-    } else {
-        printLn("Account not found.");
+void searchAccount(string name) {
+    vector<string> account = getRow(name);
+    if (account.size() == 0)
+        print("Account not found.");
+    else {
+        print("Account found!");
+        print("Name: " + account[0]);
+        print("Address: " + account[1]);
+        print("Account Type: " + account[2]);
+        print("Balance: $" + account[3]);
     }
 }
-
-int main() {
-    map<string, Account> accounts;
-    string answer;
-    print("-------------------------------------------------------");
-    print("Welcome to Gian's & Elijah's Bank Management System!");
-    print("-------------------------------------------------------");
-    answer = getStr("Would you like to create an account? (yes/no)");
-    if (answer == "no") {
-        print("No problem. Let us know if you changed your mind.");
-        return 0;
-    }
-
+string createAccount() {
     Account newAccount;
+    bool isSuccess;
     print("Great! Let's get started with your account creation process.");
     newAccount.name = getStr("Please enter your name: \n");
     newAccount.address = getStr("Enter your address: \n");
     newAccount.accountType = getStr("Enter your account type: \n");
     newAccount.amount = getNum("Enter amount: \n");
-    accounts[newAccount.name] = newAccount;
-    print("Account created successfully!");
-
-    while (true) {
-        print("\nChoose an option:");
+    string content = newAccount.name + SEPERATOR + newAccount.address + SEPERATOR + newAccount.accountType + SEPERATOR + to_string(newAccount.amount);
+    isSuccess = appendFile(fileName, content + "\n");
+    if (isSuccess) {
+        csvData[newAccount.name] = {newAccount.name, newAccount.address, newAccount.accountType, to_string(newAccount.amount)};
+        print("Account created successfully!");
+        return "";
+    }
+    return newAccount.name;
+}
+int main() {
+    vector<int> indexes = {0};
+    init(fileName, csvData, indexes);
+    string answer;
+    bool exited = false;
+    vector<string> account;
+    print("--------------------------------------------------------");
+    print("||Welcome to Gian's & Elijah's Bank Management System!||");
+    print("--------------------------------------------------------");
+    answer = getStr("Would you like to create an account? (yes/no)");
+    if (answer == "no") {
+        print("No problem. Let us know if you changed your mind.");
+        while (!exited) {
+            print("Choose an option:");
+            print("1. Create an account");
+            print("4. Search for an account");
+            print("5. Exit");
+            int choice = getNum("Enter your choice: ");
+            switch (choice) {
+                case 1:
+                    account = csvData[createAccount()];
+                    exited = account.size() > 0;
+                    break;
+                case 4: {
+                    string searchName = getStr("Enter the name to search for: ");
+                    searchAccount(searchName);
+                    break;
+                }
+                case 5: {
+                    print("Exiting the program. Thank you!");
+                    exited = true;
+                    exit(1);
+                }
+                default: {
+                    print("Invalid choice. Please try again.");
+                }
+            }
+        }
+    } else {
+        createAccount();
+    }
+    exited = false;
+    while (!exited) {
+        print("Choose an option:");
         print("1. Show account details");
         print("2. Deposit");
         print("3. Withdraw");
         print("4. Search for an account");
         print("5. Exit");
-
         int choice = getNum("Enter your choice: ");
-
-        if (choice == 5) {
-            print("Exiting the program. Thank you!");
-            break;
-        }
-
-        if (choice == 4) {
-            string searchName = getStr("Enter the name to search for: ");
-            searchContact(accounts, searchName);
-            continue;
-        }
-
-        string accountName;
-        print("Enter account name: ");
-        getline(cin, accountName);
-
-        auto it = accounts.find(accountName);
-        if (it != accounts.end()) {
-            Account& selectedAccount = it->second;
-
-            switch (choice) {
-                case 1:
-                    showAccountDetails(selectedAccount);
-                    break;
-                case 2: {
-                    double depositAmount = getNum("Enter the deposit amount: $");
-                    deposit(selectedAccount, depositAmount);
-                    break;
-                }
-                case 3: {
-                    double withdrawAmount = getNum("Enter the withdrawal amount: $");
-                    withdraw(selectedAccount, withdrawAmount);
-                    break;
-                }
-                default:
-                    printLn("Invalid choice. Please try again.");
+        switch (choice) {
+            case 1:
+                showAccountDetails(account);
+                break;
+            case 2: {
+                double depositAmount = getNum("Enter the deposit amount: $");
+                deposit(account, depositAmount);
+                break;
             }
-        } else {
-            printLn("Account not found.");
+            case 3: {
+                double withdrawAmount = getNum("Enter the withdrawal amount: $");
+                withdraw(account, withdrawAmount);
+                break;
+            }
+            case 4: {
+                string searchName = getStr("Enter the name to search for: ");
+                vector<string> account = getRow(searchName);
+                if (account.size() == 0)
+                    print("Account not found.");
+                else
+                    showAccountDetails(account);
+                break;
+            }
+            case 5: {
+                print("Exiting the program. Thank you!");
+                exited = true;
+                break;
+            }
+            default:
+                printLn("Invalid choice. Please try again.");
         }
     }
 
