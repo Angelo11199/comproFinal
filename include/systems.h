d8fe4c49c0e930992e3d2bfd462f8a1a672ddf7a
iceCream <142316765+Angelo11199@users.noreply.github.com>
2023-11-30 09:00:03 -0500

    added bank management

---
+++ include/systems.h
@@ -0,0 +1,8 @@
+
+#include "../src/bankManagement.cpp"
+#ifndef FIRST_H
+#define FIRST_H
+
+int startBankManagement();
+
+#endif
--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,8 @@
 
 #include "../include/snippets.h"  //ALWAYS ADD THIS
 using namespace std;
+#include "../include/systems.h"
 int main() {
-
+    startBankManagement();
     return 0;
 }
